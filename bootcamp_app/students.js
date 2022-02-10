1d347d5011f7d1e9d3f82308752c73170600e9fd
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-09 21:05:09 -0500

    Added parameterized queries to prevent SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,17 +7,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
 pool.connect(() => {
   console.log('connected to database');
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort.`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.connect(() => {
-  console.log('connected to database');
-});
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+const values = [process.argv[2] || 'JUL02'];
+
+pool.connect(() => {
+  console.log('connected to database');
+});
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(teacher => {
       console.log(`${teacher.cohort}: ${teacher.teacher}`);
