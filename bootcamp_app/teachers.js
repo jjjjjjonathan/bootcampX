4dd8fecbae4773203ee992b56e73e9f0ba93955e
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-09 20:21:55 -0500

    Created new queries with node postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.connect(() => {
+  console.log('connected to database');
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
+`)
+  .then(res => {
+    res.rows.forEach(teacher => {
+      console.log(`${teacher.cohort}: ${teacher.teacher}`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
